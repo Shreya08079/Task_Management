cbeb7d8b941158c33b3eedf52833786da72aa5ba
Shreya <shreya08079@gail.com>
2025-02-22 23:26:13 +0530

    Committed

--- Task_Management_Backend/app.js
+++ Task_Management_Backend/app.js
@@ -12,16 +12,17 @@ const taskRoutes = require("./routes/taskRoutes");
 const app = express();
 const PORT = process.env.PORT || 1814;
 
-// Middlewares
 app.use(morgan("dev"));
-app.use(cors({ origin: "http://localhost:5173", credentials: true }));
+app.use(cors({ origin: "https://shreya-task-management.netlify.app", credentials: true }));
+// app.use(cors({
+//     origin: process.env.FRONTEND_URL,
+//     credentials: true
+//   }));
 app.use(express.json());
 app.use(cookieParser());
 
-// Routes
 app.use("/users", authRoutes);
 app.use("/otps", otpRoutes);
 app.use("/tasks", taskRoutes);
 
-// Start server
 app.listen(PORT, () => console.log(`Server running on port: ${PORT}`));
